70beedea5464529c8d69965c4c83498706edd213
Unchanted <omkkdwivedi@gmail.com>
2024-08-14 17:04:05 +0530

    [39

--- src/js/script.js
+++ src/js/script.js
@@ -113,72 +113,78 @@
   setInterval(updateOrbs, 1000 / frames); // 24 frames per second
 
   // reposition to target orb
-  /
-function targetOrb(_id = id) {
-  const orb = orbs[_id];
-  targetX = orb.ix;
-  targetY = orb.iy;
-  targetZ = orb.iz;
-  id = _id;
-}
-
-// Set initial orb
-targetOrb();
-
-// Window resize and orientation change event listeners
-const resizeHandler = () => {
-  centerX = window.innerWidth / 2;
-  centerY = window.innerHeight / 2;
-};
-
-['resize', 'onorientationchange'].forEach(eventType =>
-  window.addEventListener(eventType, resizeHandler)
-);
-
-// Move to clicked orb
-function clickedOrb(event) {
-  const targetId = Number(event.target.dataset.id);
-  if (id !== targetId) {
-    targetOrb(targetId);
+  function targetOrb(_id) {
+    _id = _id === undefined ? id : _id;
+    targetX = orbs[_id].ix;
+    targetY = orbs[_id].iy;
+    targetZ = orbs[_id].iz;
+    id = _id;
   }
-}
 
-// Mouse wheel support
-window.addEventListener('wheel', event => {
-  const delta = Math.sign(event.deltaY);
-  if (delta === -1 && id > 0) {
-    targetOrb(--id); // Previous orb
-  } else if (delta === 1 && id < orbTotal - 1) {
-    targetOrb(++id); // Next orb
-  }
-});
-
-// Keyboard shortcuts
-window.addEventListener('keydown', event => {
-  const { which } = event;
-  const keys = { up: 38, down: 40, left: 37, right: 39, space: 32 };
-
-  if (which === keys.up || which === keys.space) {
-    if (id < orbTotal - 1) targetOrb(++id); // Next orb
-  } else if (which === keys.down && id > 0) {
-    targetOrb(--id); // Previous orb
-  } else if (which === keys.left) {
-    targetOrb(0); // First orb
-  } else if (which === keys.right) {
-    targetOrb(orbTotal - 1); // Last orb
-  }
-});
-
-// Random pattern event listener
-document.querySelector('.patterns__random').addEventListener('click', event => {
-  event.preventDefault();
-  randomPattern();
+  // set initial orb
   targetOrb();
-});
 
-function randomPattern() {
-  orbs.forEach(orb => {
-    orb.ix = getRandomRange(0, window.innerWidth - orbWidth);
-    orb.iy = getRandomRange(0, window.innerHeight - orbHeight);
+  // window resize events
+  ['resize', 'onorientationchange'].forEach(item => {
+    window.addEventListener(item, event => {
+      centerX = window.innerWidth * 0.5;
+      centerY = window.innerHeight * 0.5;
+    });
   });
-}
+
+  // move to clicked orb
+  function clickedOrb(event) {
+    const targetId = event.target.dataset.id;
+    if (id !== targetId) {
+      id = targetId;
+      targetOrb();
+    }
+  }
+
+  // mouse wheel support
+  window.addEventListener('wheel', event => {
+    const delta = 1 * Math.sign(event.deltaY);
+    if (id > 0 && delta === -1) {
+      targetOrb(--id); // previous orb
+    } else if (id < (orbTotal - 1) && delta === 1) {
+      targetOrb(++id); // next orb
+    }
+  });
+
+  // keyboard shortcuts
+  window.addEventListener('keydown', event => {
+    let keys = {
+      up: 38,
+      down: 40,
+      left: 37,
+      right: 39,
+      space: 32
+    };
+    switch (event.which) {
+      case keys.up:
+      case keys.space:
+        return id < (orbTotal - 1) && targetOrb(++id); // next orb
+      case keys.down:
+        return id > 0 && targetOrb(--id); // previous orb
+      case keys.left:
+        return targetOrb(0); // first orb
+      case keys.right:
+        return targetOrb(orbTotal - 1); // last orb
+    }
+
+  });
+
+  // random pattern
+  document.querySelector('.patterns__random').addEventListener('click', event => {
+    event.preventDefault();
+    randomPattern();
+    targetOrb();
+  });
+
+  function randomPattern() {
+    for (let i = 0; i < orbTotal; i++) {
+      orbs[i].ix = getRandomRange(0, window.innerWidth - orbWidth);
+      orbs[i].iy = getRandomRange(0, window.innerHeight - orbHeight);
+    }
+  }
+
